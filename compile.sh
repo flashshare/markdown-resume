cf04b6fc0c1c6bf763a3c04cd7c0bf102ed159fc
Nguyen Truong An <79207918+flashshare@users.noreply.github.com>
2025-04-25 07:18:34 +0200

    Update compile avoid  Double quote to prevent globbing and word splitting.

--- compile.sh
+++ compile.sh
@@ -31,11 +31,10 @@ else
   echo "using '$STYLE' style" >&2
 fi
 
-
 source_base=$(basename "$sourcefile" .md)
 
 # shellcheck disable=SC2012
-styles=$(ls -p styles/${STYLE}/*.css | sed "s/^/-c /" | tr "\n" " ")
+styles=$(ls -p styles/"${STYLE}"/*.css | sed "s/^/-c /" | tr "\n" " ")
 
 # we actually want the spaces to split out the args
 # shellcheck disable=SC2086
